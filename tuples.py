3992cbb129b61cd6700c9e66fc0571fa6b5a9d4e
Kush Mishra <kushmishra05@gmail.com>
2021-12-10 21:15:07 +0530

    Tuples

---
+++ tuples.py
@@ -0,0 +1,12 @@
+tuple1 = (1, 2, 3)
+
+# Repetition Allowed
+tuple2 = tuple((1, 2, 3, 1))
+
+# Allows Various datatypes
+tuple3 = ('strings', 4, True)
+print(tuple3)
+
+# Get type
+print(type(tuple3[1]))
+print(type(tuple1))
